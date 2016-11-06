004f50a02e572605ddc7714c61b72b1489dca25a
shelzebub <makaxsole@gmail.com>
2016-11-06 00:44:45 -0700

    so much stuff

--- js/house.js
+++ js/house.js
@@ -1,40 +1,98 @@
-    
+    //MAIN BUILDING TEMPLATE
     //front house wall
     var xLeft = 150;
-    var xRight = 250; 
+    var xRight = 250;
     var yTop = 275;
     var yBot = 415;
     //transition of house walls facing me
-    var xChange = 75; 
+    var xChange = 50;
     var yChange = 50;
     //tallnedd of triangle part of wall
-    var tip = -75;
+    var tip = -75
 
-    //back house wall 
+    //back house wall
     var xLeft2 = xLeft + xChange;
-    var xRight2 = xRight + xChange; 
+    var xRight2 = xRight + xChange;
     var yTop2 = yTop - yChange;
     var yBot2 = yBot - yChange;
 
-    
+
     //triangle part front wall
     var xTopOfTriangle = ((xRight - xLeft) / 2)+ xLeft;
     var yTopOfTriangle  = yTop + tip;
-    
+
     //triangle partback wall
     var xTopOfTriangle2 = ((xRight2 - xLeft2) / 2)+ xLeft2;
     var yTopOfTriangle2  = yTop2 + tip;
-    
-    
+    //----------------------------------------------------
+
+    //SMALL ENTRANCE BUILDING TEMPLATE
+    //front wall
+    var xLeftSm = 150+5;
+    var xRightSm = 250-5;
+    var yTopSm = 178+275*4/7;
+    var yBotSm = 178+415*4/7;
+
+    //transition of house walls facing me
+    var xChangeSm = -23;
+    var yChangeSm = -23;
+    //tallnedd of triangle part of wall
+    var tipSm = -62;
+
+    //back house wall
+    var xLeftSm2 = xLeftSm + xChangeSm;
+    var xRightSm2 = xRightSm + xChangeSm;
+    var yTopSm2 = yTopSm - yChangeSm;
+    var yBotSm2 = yBotSm - yChangeSm;
+
+
+    //triangle part front wall
+    var xTopOfTriangleSm = ((xRightSm - xLeftSm) / 2)+ xLeftSm;
+    var yTopOfTriangleSm  = yTopSm + tipSm;
+
+    //triangle partback wall
+    var xTopOfTriangleSm2 = ((xRightSm2 - xLeftSm2) / 2)+ xLeftSm2;
+    var yTopOfTriangleSm2  = yTopSm2 + tipSm;
+
+    //---------------------------------------------------------
+
+
+
+   //roof
+    var xMidRoofLeft = ((xRight - xLeft) / 2)+ xLeft -7;
+    var yMidRoofLeft = yTop + tip -10;
+    var xMidRoofRight = ((xRight - xLeft) / 2)+ xLeft -7 + xChange +5;
+    var yMidRoofRight = yTop + tip -10 - yChange -5;
+    var xBotLeft = xRight +7;
+    var yBotLeft = yTop +10;
+    var xBotRight = xBotLeft + xChange +5;
+    var yBotRight = yBotLeft - yChange -5;
+
+
+
+    //entrance roof
+    var xMidRoofLeftEntrance = xMidRoofLeft-15-5;
+    var yMidRoofLeftEntrance = yMidRoofLeft+100;
+    var xMidRoofRightEntrance = xMidRoofRight-45-5;
+    var yMidRoofRightEntrance = yMidRoofRight+130;
+    var xBotLeftEntrance = xBotLeft-15;
+    var yBotLeftEntrance = yBotLeft+80;
+    var xBotRightEntrance = xBotRight-45;
+    var yBotRightEntrance = yBotRight+110;
+
+
+
+
+
+
     
 
 function frontWall() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-  
+
     //bottom rectangle
-    
-    
+
     ctx.beginPath();
     ctx.fillStyle = "#ffddcc";
     ctx.beginPath();
@@ -42,23 +100,23 @@ function frontWall() {
     ctx.lineTo(xLeft,yBot);
     ctx.lineTo(xRight,yBot);
     ctx.lineTo(xRight,yTop);
-    //ctx.fill();
-    ctx.stroke();
+    ctx.fill();
+   // ctx.stroke();
     ctx.closePath();
-    
-    
+
+
     //triangle part
-    
-    
+
+
    // ctx.beginPath();
     ctx.moveTo(xLeft,yTop);
     ctx.lineTo(xTopOfTriangle, yTopOfTriangle);
     ctx.lineTo(xRight,yTop);
-    ctx.stroke();
-    //ctx.fill();
+    //ctx.stroke();
+    ctx.fill();
     ctx.closePath();
-    
-    
+
+
 
 }
 
@@ -66,9 +124,9 @@ function backWall(){
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
 
-   
+
     //bottom rectangle
-    
+
     ctx.beginPath();
     ctx.fillStyle = "#ffddcc";
     ctx.beginPath();
@@ -76,211 +134,392 @@ function backWall(){
     ctx.lineTo(xLeft2,yBot2);
     ctx.lineTo(xRight2,yBot2);
     ctx.lineTo(xRight2,yTop2);
-    //ctx.fill();
-    ctx.stroke();
+    ctx.fill();
+    //ctx.stroke();
     ctx.closePath();
-    
-    
+
+
     //triangle part
-   // ctx.beginPath();
+    // ctx.beginPath();
     ctx.moveTo(xLeft2,yTop2);
     ctx.lineTo(xTopOfTriangle2, yTopOfTriangle2);
     ctx.lineTo(xRight2,yTop2);
-    ctx.stroke()
-    //ctx.fill();
+    //ctx.stroke()
+    ctx.fill();
     ctx.closePath();
 }
-
 
 function sideBaseRoofRight(){
     var c = document.getElementById("myCanvas");
     var ctx= c.getContext("2d");
-    
+
     ctx.beginPath();
+    ctx.fillStyle = "#ffddcc";
+
     ctx.moveTo(xRight, yTop);
     ctx.lineTo(xRight2, yTop2);
     ctx.lineTo(xTopOfTriangle2, yTopOfTriangle2);
     ctx.lineTo(xTopOfTriangle, yTopOfTriangle);
     ctx.lineTo(xRight, yTop);
 
-    //ctx.fill();
-    ctx.stroke();
+    ctx.fill();
+    //ctx.stroke();
+    ctx.closePath();
+
+}
+
+function sideRoofRight(){
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    
+    ctx.beginPath();
+    ctx.fillStyle = "#fdac84";
+
+    ctx.moveTo(xMidRoofLeft, yMidRoofLeft); //bottom left
+    ctx.lineTo(xBotLeft-3, yBotLeft);
+    ctx.lineTo(xBotRight-3, yBotRight);
+    ctx.lineTo(xMidRoofRight,yMidRoofRight+5);
+    ctx.lineTo(xMidRoofLeft, yMidRoofLeft+5);
+  
+    ctx.fill();
+    //ctx.stroke();
     ctx.closePath();
     
+  ctx.beginPath();
+    ctx.moveTo(xMidRoofLeft, yMidRoofLeft); //bottom left
+    ctx.lineTo(xBotLeft, yBotLeft);
+    ctx.lineTo(xBotRight, yBotRight);
+    ctx.lineTo(xMidRoofRight,yMidRoofRight);
+    ctx.lineTo(xMidRoofLeft, yMidRoofLeft);
+
+    ctx.fill();
+    //ctx.stroke();
+    ctx.closePath();
+
 }
 
 function sideBaseRoofLeft(){
     var c = document.getElementById("myCanvas");
     var ctx= c.getContext("2d");
-    
+
     ctx.beginPath();
+    ctx.fillStyle = "#fdac84";
+
     ctx.moveTo(xLeft, yTop);
     ctx.lineTo(xLeft2, yTop2);
     ctx.lineTo(xTopOfTriangle2, yTopOfTriangle2);
     ctx.lineTo(xTopOfTriangle, yTopOfTriangle);
     ctx.lineTo(xLeft, yTop);
 
-    //ctx.fill();
-    ctx.stroke();
+    ctx.fill();
+    //ctx.stroke();
     ctx.closePath();
-        
-     
+
 }
+
+function sideRoofLeft(){
+ 
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#d37749";
+
+    ctx.moveTo(xMidRoofLeft, yMidRoofLeft); //bottom left
+    ctx.lineTo(xBotLeft-120, yBotLeft);
+    ctx.lineTo(xBotRight-120, yBotRight);
+    ctx.lineTo(xMidRoofRight,yMidRoofRight);
+    ctx.lineTo(xMidRoofLeft, yMidRoofLeft);
+    
+    
+    
+
+    ctx.fill();
+   // ctx.stroke();
+    ctx.closePath();
+    ctx.beginPath();
+    
+   
+    ctx.moveTo(xMidRoofLeft, yMidRoofLeft+5); //bottom left
+    ctx.lineTo(xBotLeft-120+3, yBotLeft);
+    ctx.lineTo(xBotRight-120+3, yBotRight);
+    ctx.lineTo(xMidRoofRight,yMidRoofRight+5);
+    ctx.lineTo(xMidRoofLeft, yMidRoofLeft);
+   
+
+    ctx.fill();
+ //   ctx.stroke();
+    ctx.closePath();
+
+}
+
+function roofThingy() {
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#fdac84";
+
+    ctx.moveTo(xMidRoofLeft, yMidRoofLeft); //bottom left
+    ctx.lineTo(xMidRoofLeft, yMidRoofLeft-10); //top left
+    ctx.lineTo(xMidRoofRight, yMidRoofRight-10);
+    ctx.lineTo(xMidRoofRight, yMidRoofRight);
+    
+    ctx.fill();
+ //   ctx.stroke();
+    ctx.closePath();
+}
+
 
 function sideWallRight(){
     var c = document.getElementById("myCanvas");
     var ctx= c.getContext("2d");
-    
+
     ctx.beginPath();
+    ctx.fillStyle = "#ce997e";
+
     ctx.moveTo(xRight, yTop);
     ctx.lineTo(xRight2, yTop2);
     ctx.lineTo(xRight2, yBot2);
     ctx.lineTo(xRight, yBot);
     ctx.lineTo(xRight, yTop);
 
-    //ctx.fill();
-    ctx.stroke();
+    ctx.fill();
+  //  ctx.stroke();
     ctx.closePath();
-         
-}
 
-function sideWallRight(){
-    var c = document.getElementById("myCanvas");
-    var ctx= c.getContext("2d");
-    
-    ctx.beginPath();
-    ctx.moveTo(xRight, yTop);
-    ctx.lineTo(xRight2, yTop2);
-    ctx.lineTo(xRight2, yBot2);
-    ctx.lineTo(xRight, yBot);
-    ctx.lineTo(xRight, yTop);
-
-    //ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-         
 }
 
 function sideWallLeft(){
     var c = document.getElementById("myCanvas");
     var ctx= c.getContext("2d");
-    
+
     ctx.beginPath();
+    ctx.fillStyle = "#ffddcc";
+
     ctx.moveTo(xLeft, yTop);
     ctx.lineTo(xLeft2, yTop2);
     ctx.lineTo(xLeft2, yBot2);
     ctx.lineTo(xLeft, yBot);
     ctx.lineTo(xLeft, yTop);
 
-    //ctx.fill();
-    ctx.stroke();
+    ctx.fill();
+  //  ctx.stroke();
     ctx.closePath();
-         
+
 }
 
-function main() {
-    //Run main program here.
-    sideWallLeft()
+//------------------------------------------------------------------------
+//entrance building
+function backWallEntrance() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    //bottom rectangle
+
+    ctx.beginPath();
+    ctx.fillStyle = "#ffddcc";
+    ctx.beginPath();
+    ctx.moveTo(xLeftSm,yTopSm);
+    ctx.lineTo(xLeftSm,yBotSm);
+    ctx.lineTo(xRightSm,yBotSm);
+    ctx.lineTo(xRightSm,yTopSm);
+    ctx.fill();
+//    ctx.stroke();
+    ctx.closePath();
+
+    //triangle part
+
+    ctx.beginPath();
+    ctx.moveTo(xLeftSm,yTopSm);
+    ctx.lineTo(xTopOfTriangleSm, yTopOfTriangleSm);
+    ctx.lineTo(xRightSm,yTopSm);
+  //  ctx.strokeStyle = "blue";
+  //  ctx.stroke();
+    //ctx.fill();
+    ctx.closePath();
+}
+
+function frontWallEntrance() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+
+    //bottom rectangle
+
+    ctx.beginPath();
+    ctx.fillStyle = "#ffddcc";
+    ctx.beginPath();
+    ctx.moveTo(xLeftSm2,yTopSm2);
+    ctx.lineTo(xLeftSm2,yBotSm2);
+    ctx.lineTo(xRightSm2,yBotSm2);
+    ctx.lineTo(xRightSm2,yTopSm2);
+    ctx.fill();
+  //  ctx.stroke();
+    ctx.closePath();
+
+
+    //triangle part
+    ctx.beginPath();
+    ctx.moveTo(xLeftSm2,yTopSm2);
+    ctx.lineTo(xTopOfTriangleSm2, yTopOfTriangleSm2);
+    ctx.lineTo(xRightSm2,yTopSm2);
+  //  ctx.stroke()
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+function sideBaseRoofRightEntrance(){
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#ffddcc";
+
+    ctx.moveTo(xRightSm, yTopSm);
+    ctx.lineTo(xRightSm2, yTopSm2);
+    ctx.lineTo(xTopOfTriangleSm2, yTopOfTriangleSm2);
+    ctx.lineTo(xTopOfTriangleSm, yTopOfTriangleSm);
+    ctx.lineTo(xRightSm, yTopSm);
+
+    //ctx.fill();
+//    ctx.stroke();
+    ctx.closePath();
+
+}
+
+function sideRoofRightEntrance() {
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#fdac84";
+    ctx.moveTo(xMidRoofLeftEntrance, yMidRoofLeftEntrance); //bottom left
+    ctx.lineTo(xBotLeftEntrance-15, yBotLeftEntrance);
+    ctx.lineTo(xBotRightEntrance-15, yBotRightEntrance);
+    ctx.lineTo(xMidRoofRightEntrance,yMidRoofRightEntrance);
+    ctx.lineTo(xMidRoofLeftEntrance, yMidRoofLeftEntrance);
+    
+
+    ctx.fill();
+  //  ctx.stroke();
+    ctx.closePath();
+
+}
+
+function sideBaseRoofLeftEntrance(){
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#d37749";
+
+    
+    ctx.moveTo(xLeftSm, yTopSm);
+    ctx.lineTo(xLeftSm2, yTopSm2);
+    ctx.lineTo(xTopOfTriangleSm2, yTopOfTriangleSm2);
+    ctx.lineTo(xTopOfTriangleSm, yTopOfTriangleSm);
+    ctx.lineTo(xLeftSm, yTopSm);
+
+    //ctx.fill();
+  //  ctx.stroke();
+    ctx.closePath();
+
+
+}
+
+function sideRoofLefttEntrance() {
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#d37749";
+
+    ctx.moveTo(xMidRoofLeftEntrance, yMidRoofLeftEntrance); //bottom left
+    ctx.lineTo(xBotLeftEntrance-120, yBotLeftEntrance);
+    ctx.lineTo(xBotRightEntrance-120, yBotRightEntrance);
+    ctx.lineTo(xMidRoofRightEntrance,yMidRoofRightEntrance);
+    ctx.lineTo(xMidRoofLeftEntrance, yMidRoofLeftEntrance);
+    
+
+    ctx.fill();
+  //  ctx.stroke();
+    ctx.closePath(); 
+
+
+}
+
+
+
+
+function sideWallRightEntrance(){
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "#ffc2a3";
+
+    ctx.moveTo(xRightSm, yTopSm);
+    ctx.lineTo(xRightSm2, yTopSm2);
+    ctx.lineTo(xRightSm2, yBotSm2);
+    ctx.lineTo(xRightSm, yBotSm);
+    ctx.lineTo(xRightSm, yTopSm);
+
+    ctx.fill();
+  //  ctx.stroke();
+    ctx.closePath();
+
+}
+
+function sideWallLeftEntrance(){
+    var c = document.getElementById("myCanvas");
+    var ctx= c.getContext("2d");
+
+    ctx.beginPath();
+    ctx.moveTo(xLeftSm, yTopSm);
+    ctx.lineTo(xLeftSm2, yTopSm2);
+    ctx.lineTo(xLeftSm2, yBotSm2);
+    ctx.lineTo(xLeftSm, yBotSm);
+    ctx.lineTo(xLeftSm, yTopSm);
+
+    //ctx.fill();
+   // ctx.stroke();
+    ctx.closePath();
+
+}
+
+function mainBuilding(){
+    sideWallLeft();
     sideBaseRoofLeft();
     backWall();
-    frontWall();
     sideBaseRoofRight();
     sideWallRight();
+    sideRoofLeft();
+    roofThingy();
+    frontWall();
+    sideRoofRight();
+}
+
+function entranceBuilding(){
+
     
-   
-    
-
-}
-
-
-/*
-
-function houseFront() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.fillStyle = "#61316f";
-    ctx.fillRect(100, 200+50, 175, 125);
-    ctx.fillStyle = "#582567";
-    ctx.moveTo(275, 200+50);
-    ctx.lineTo(325, 150+50);
-    ctx.lineTo(325, 275+50);
-    ctx.lineTo(275, 325+50);
-    ctx.closePath();
-    ctx.fill();
-
-}
-
-function houseBack() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.fillStyle = "#6a317b";
-    ctx.fillRect(150, 150+50, 175, 125);
-    ctx.moveTo(100, 200+50);
-    ctx.lineTo(150, 150+50);
-    ctx.lineTo(150, 275+50);
-    ctx.lineTo(100, 325+50);
-    ctx.closePath();
-    ctx.fill();
-
-}
-
-function roofArc() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle = "#514d52";
-    ctx.beginPath();
-    ctx.arc(187.5, 250, 100, Math.PI * 1, 0, false);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.arc(237.5, 150+50, 100, Math.PI * 1, 0, false);
-    ctx.closePath();
-    ctx.fill();
-    
-    ctx.fillStyle = "#443f45";;
-    ctx.beginPath();
-    ctx.moveTo(287.5,200+50);
-    ctx.lineTo(337.5,150+50);
-    ctx.arc(237.5, 150+50, 100, 0, (Math.PI* 5 /4), true);
-    ctx.arc(187.5, 200+50, 100, (Math.PI* 5 /4), 0, false);
-    ctx.closePath();
-    ctx.fill();
-}
-
-
-
-function chimney() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.fillStyle = "#e574d4";
-    ctx.fillRect(155, 100 , 50, 75);
-}
-
-function houseDoor(){
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle = "#aae574";
-    ctx.fillRect(155, 300 , 50, 75);
-}
-
-function houseBase() {
-    houseBack();
-    houseFront();
-    roofArc();
-    houseDoor();
+    backWallEntrance();
+    sideWallLeftEntrance();
+    sideBaseRoofLeftEntrance();
+    sideBaseRoofRightEntrance();
+    sideWallRightEntrance();
+    sideRoofLefttEntrance();
+    frontWallEntrance();
+    sideRoofRightEntrance();
     
 }
 
 function main() {
     //Run main program here.
-
-    houseBase();
-    chimney();
+  
+    mainBuilding();
+    entranceBuilding();
 
 }
-
-*/
